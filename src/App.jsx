55667c1bb9ff7bcef5e9eed12e9abecc9be5fda8
Manel Soler <manelsoler@gmail.com>
2023-11-15 10:38:10 +0100

    modulating the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
 import { useState } from "react"
 import confetti from "canvas-confetti"
-
-const TURNS = {
-  X: 'x',
-  O:'o'
-}
-
-const WINNER_COMBOS = [
-  [0,1,2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+import { Square } from "./component/Square"
+import { TURNS, WINNER_COMBOS } from "./constants" 
 
 
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-  
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children} 
-    </div>
-  )
-} 
-  
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null))
